3cd469bb89f97e6736c0ad15a2618f16d0eb8c03
Unknown <m.pugliese@murdoch.edu.au>
2018-09-27 11:37:56 +0800

    First version a Chrome extension which I've quickly smacked together so I can easily add local file links to my Trello cards.
    Made from snippets and examples of JavaScript I found in message boards. I've left urls referring to those pages in the source code.

---
+++ background.js
@@ -0,0 +1,65 @@
+'use strict';
+
+// A gotcha of sorts with chrome extensions involving clipboard actions is that
+// only the content scripts can interact with the page that a user loads. This
+// means that we can't put our calls to actually paste into the page in the
+// background file, because the background scripts are not able to paste into
+// the dom of the page. However, only background pages are able to access the
+// system clipboard. Therefore we have to do a little trickery to move between
+// the two. We're going to define the functions here to actually read from the
+// clipboard into a div, and then
+// we'll get that pasted data from the background page and do the actual
+// insertion in our content script. The idea of this comes from:
+// https://stackoverflow.com/questions/6969403/cant-get-execcommandpaste-to-work-in-chrome
+/**
+ * Retrieve the current content of the system clipboard.
+ */
+function getContentFromClipboard() {
+    var pasteTarget = document.createElement("div");
+    pasteTarget.contentEditable = true;
+    var actElem = document.activeElement.appendChild(pasteTarget).parentNode;
+    pasteTarget.focus();
+    document.execCommand("Paste", null, null);
+    var paste = pasteTarget.innerText;
+    actElem.removeChild(pasteTarget);
+    return paste;
+}
+
+/**
+ * Send the value that should be pasted to the content script.
+ */
+function sendPasteToContentScript(toBePasted) {
+    // We first need to find the active tab and window and then send the data
+    // along. This is based on:
+    // https://developer.chrome.com/extensions/messaging
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {data: toBePasted});
+    });
+}
+
+/**
+ * The function that will handle our context menu clicks.
+ */
+function onClickHandler(info, tab) {
+    var clipboardContent = getContentFromClipboard();
+    if (info.menuItemId === 'pasteLocalFileLocationIntoTrello') 
+	{
+		// turn into file metadata
+		// [c://<file name>](file://c:/file%20name)
+		var link = "[" + clipboardContent.replace(/\\/g, "\\\\") + "](file://" + clipboardContent.replace(/ /g, "%20") + ")";
+        sendPasteToContentScript(link);
+    }
+}
+
+// Register the click handler for our context menu.
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// Set up the single one item "paste"
+chrome.runtime.onInstalled.addListener(function(details) {
+    chrome.contextMenus.create(
+        {
+            'title': 'Paste Local File Location',
+            'id': 'pasteLocalFileLocationIntoTrello',
+            'contexts': ['editable']
+        });
+});
---
+++ content.js
@@ -0,0 +1,72 @@
+'use strict';
+
+// From : https://stackoverflow.com/questions/3964710/replacing-selected-text-in-the-textarea
+function getInputSelection(el) {
+    var start = 0, end = 0, normalizedValue, range,
+        textInputRange, len, endRange;
+
+    if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") {
+        start = el.selectionStart;
+        end = el.selectionEnd;
+    } else {
+        range = document.selection.createRange();
+
+        if (range && range.parentElement() == el) {
+            len = el.value.length;
+            normalizedValue = el.value.replace(/\r\n/g, "\n");
+
+            // Create a working TextRange that lives only in the input
+            textInputRange = el.createTextRange();
+            textInputRange.moveToBookmark(range.getBookmark());
+
+            // Check if the start and end of the selection are at the very end
+            // of the input, since moveStart/moveEnd doesn't return what we want
+            // in those cases
+            endRange = el.createTextRange();
+            endRange.collapse(false);
+
+            if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
+                start = end = len;
+            } else {
+                start = -textInputRange.moveStart("character", -len);
+                start += normalizedValue.slice(0, start).split("\n").length - 1;
+
+                if (textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
+                    end = len;
+                } else {
+                    end = -textInputRange.moveEnd("character", -len);
+                    end += normalizedValue.slice(0, end).split("\n").length - 1;
+                }
+            }
+        }
+    }
+
+    return {
+        start: start,
+        end: end
+    };
+}
+
+function replaceSelectedText(el, text) {
+    var sel = getInputSelection(el), val = el.value;
+    el.value = val.slice(0, sel.start) + text + val.slice(sel.end);
+}
+
+/**
+ * Insert the text at the cursor into the active element. Note that we're
+ * intentionally not appending or simply replacing the value of the editable
+ * field, as we want to allow normal pasting conventions. If you paste at the
+ * beginning, you shouldn't see all your text be replaced.
+ * Taken from:
+ * http://stackoverflow.com/questions/7404366/how-do-i-insert-some-text-where-the-cursor-is
+ */
+function insertTextAtCursor(text) {
+    var el = document.activeElement;
+    replaceSelectedText(el, text);
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.data) {
+        insertTextAtCursor(request.data);
+    }
+});
